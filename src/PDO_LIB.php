a9bca5abff580fccc871ae4161639e14839fea10
Yuri Frantsevich <frantsevich@gmail.com>
2021-10-19 10:04:41 +0300

    Added Readme file

--- src/PDO_LIB.php
+++ src/PDO_LIB.php
@@ -264,10 +264,9 @@ class PDO_LIB extends AbstractDB {
                 $connect_line = "mysql:host=".$this->db_host.";port=".$this->db_port.";dbname=".$this->db_name.";charset=".$this->db_charset;
         }
         $opt = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_WARNING,
         ];
         try {
             $this->db_connect = new PDO($connect_line, $this->db_user, $this->db_pass, $opt);
